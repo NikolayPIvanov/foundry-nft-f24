ed437528f08782601f2648e32451ea8ba7cdf952
Nikolay Ivanov <nick.ivanov98@gmail.com>
2024-11-26 08:49:22 +0200

    Adding mint nft

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -5,12 +5,17 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
     uint256 private s_tokenCounter;
+    mapping(uint256 => string) private s_tokenIdToUri;
 
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
         s_tokenCounter = 0;
     }
 
-    function mintNft() public {}
+    function mintNft(string memory tokenUri) public {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         return "";
