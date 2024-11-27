deb7219932ac26f2d9292856b2e9da958ee2773b
Nikolay Ivanov <nick.ivanov98@gmail.com>
2024-11-27 21:24:20 +0200

    Adding mint interaction

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -3,7 +3,23 @@ pragma solidity ^0.8.28;
 
 import {Script} from "forge-std/Script.sol";
 import {BasicNft} from "../src/BasicNft.sol";
+import {DevOpsTools} from "foundry-devops/DevOpsTools.sol";
 
 contract MintBasicNft is Script {
-    function run() external {}
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
+
+        mintNftOnContract(mostRecentlyDeployed);
+    }
+
+    function mintNftOnContract(address _mostRecentlyDeployed) public {
+        vm.startBroadcast();
+
+        BasicNft(_mostRecentlyDeployed).mintNft(PUG_URI);
+
+        vm.stopBroadcast();
+    }
 }
