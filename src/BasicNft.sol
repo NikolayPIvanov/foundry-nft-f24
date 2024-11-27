24fc2943e75ae403f43338ca1b82e564ccc421b7
Nikolay Ivanov <nick.ivanov98@gmail.com>
2024-11-27 21:12:33 +0200

    Adding tests

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -18,6 +18,6 @@ contract BasicNft is ERC721 {
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        return "";
+        return "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
     }
 }
