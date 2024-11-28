b03c8a558b58f1262993fdacd90fb2b3fbef4268
Nikolay Ivanov <nick.ivanov98@gmail.com>
2024-11-28 08:30:30 +0200

    Adding on-chain svg nfts

---
+++ src/MoodNft.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+    uint256 private s_tokenCounter;
+    string private s_sadSvg;
+    string private s_happySvg;
+
+    constructor(string memory _name, string memory _symbol, string memory sadSvg, string memory happySvg)
+        ERC721(_name, _symbol)
+    {
+        s_tokenCounter = 0;
+        s_sadSvg = sadSvg;
+        s_happySvg = happySvg;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return "";
+    }
+}
